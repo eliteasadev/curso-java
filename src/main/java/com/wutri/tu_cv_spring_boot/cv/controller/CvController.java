924e6911f462e2c949788d8925c65af7754ab164
eliteasadev <eliteasadev@protonmail.com>
2026-07-17 17:10:10 -0600

    doc: model & modelmap

--- src/main/java/com/wutri/tu_cv_spring_boot/cv/controller/CvController.java
+++ src/main/java/com/wutri/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,23 +1,37 @@
 package com.wutri.tu_cv_spring_boot.cv.controller;
 
+import com.wutri.tu_cv_spring_boot.cv.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/cv", method = RequestMethod.GET)
 public class CvController {
 
-    @GetMapping("/home")
-    public String index() {
+    @GetMapping({"/index", "/", "/home"})
+    public String index(Model model) {
+        Person person = new Person("Gabriel", "Moy", "Desarrollador Web");
+        model.addAttribute("name", "Gabriel");
+        model.addAttribute("person", person);
         return "index";
     }
 
+//    @GetMapping({"/index", "/", "/home"})
+//    public String index(Map<String, Object> model) {
+//        model.put("name", "Gabriel");
+//        return "index";
+//    }
+
     // @RequestMapping(value = "/pep", method = RequestMethod.GET)
     //@GetMapping({"/pep", "/", "/home"})
-    @GetMapping("/pep")
-    public String pep() {
-        return "pep";
-    }
+    //@GetMapping("/pep")
+//    public String pep() {
+//        return "pep";
+//    }
 }
